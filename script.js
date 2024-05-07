d351af3277e1d9e0630d9af83400e33b9b4f0062
simon.horrabin <simonhorrabin@gmail.com>
2024-05-07 13:47:43 -0400

    found mistake in why answers were not displaying

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function showQuestion(){
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
         if(answer.correct){
             button.dataset.correct = answer.correct;
         }
@@ -75,5 +75,16 @@ function resetState(){
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true"
+    if(isCorrect){
+        selectedBtn.classList.add('correct');
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+
 }
 startQuiz()
