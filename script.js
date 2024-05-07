2143a88163993850885d455ed4773f056cd049a9
simon.horrabin <simonhorrabin@gmail.com>
2024-05-07 13:04:52 -0400

    javascript questions rendered to page

--- script.js
+++ script.js
@@ -1,77 +1,78 @@
 const questions = [
     {
-        question:"Where does Bella live?",
+        question: "Where does Bella live?",
         answers: [
-            {text: "Narnia", correct: false},
-            {text: "Charlotte", correct: true},
-            {text: "St. Thomas", correct: false},
-            {text: "Congo", correct: false},
+            { text: "Narnia", correct: false },
+            { text: "Charlotte", correct: true },
+            { text: "St. Thomas", correct: false },
+            { text: "Congo", correct: false },
         ]
     },
-
     {
-        question:"What kind of toy is Bella's favorite?",
+        question: "What kind of toy is Bella's favorite?",
         answers: [
-            {text: "Tug-o-War", correct: true},
-            {text: "frisbee", correct: false},
-            {text: "Tennis ball", correct: false},
-            {text: "Bones", correct: false},
+            { text: "Tug-o-War", correct: true },
+            { text: "frisbee", correct: false },
+            { text: "Tennis ball", correct: false },
+            { text: "Bones", correct: false },
         ]
     },
-
     {
-        question:"What kind of dog is Bella?",
+        question: "What kind of dog is Bella?",
         answers: [
-            {text: "Lab-mix", correct: false},
-            {text: "Chihuahua", correct: true},
-            {text: "Boxer-mix", correct: false},
-            {text: "Husky", correct: false},
+            { text: "Lab-mix", correct: false },
+            { text: "Chihuahua", correct: true },
+            { text: "Boxer-mix", correct: false },
+            { text: "Husky", correct: false },
         ]
     },
-
     {
-        question:"Who is Bella's best friend?",
+        question: "Who is Bella's best friend?",
         answers: [
-            {text: "Andrew", correct: false},
-            {text: "Charlotte", correct: false},
-            {text: "Bobby", correct: false},
-            {text: "Simon", correct: true},
+            { text: "Andrew", correct: false },
+            { text: "Charlotte", correct: false },
+            { text: "Bobby", correct: false },
+            { text: "Simon", correct: true },
         ]
     },
-
 ];
-// add "buttons" to answer buttons if there is a bug
+
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
-const nextBtn = document.getElementById("next-btn");
+const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz() {
+function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
-    nextBtn.innerHTML = "Next";
+    nextButton.innerHTML = "Next";
+    showQuestion();
 }
 
-function showQuestion() {
-    let currentQuestionIndex = questions[currentQuestionIndex];
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestionIndex.question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButton.appendChild(button);
-        
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
-
 }
 
-function resetState() {
+
+function resetState(){
     nextButton.style.display = "none";
-    while(anserButtons.firstChild){
+    while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
